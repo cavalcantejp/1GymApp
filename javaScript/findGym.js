240d8d2cbd853960be7108ce12bf1730354d6565
JP <x20231857@student.ncirl.ie>
2022-07-18 14:11:07 +0100

    search bar and map on findgym and new js 4 findgym

---
+++ javaScript/findGym.js
@@ -0,0 +1,44 @@
+var db = firebase.database();
+var dbGyms = db.ref('gyms');
+
+function listGyms()
+{
+    // just an example on how to list things from real time database
+    // gonna be used on the dropdown button
+    dbGyms.on("value", function(snapshot) 
+    {
+        var gyms = "";
+        var coordinates = [];
+        snapshot.forEach(function(childSnapshot) 
+        {
+            var childData = childSnapshot.val();
+            gyms = gyms + ", " + childData.name;
+
+            //had to use parse to accept the number
+            coordinations.push({ lat: parseFloat(childData.latitude), lng: parseFloat(childData.longitude) });
+        });
+        //saves the elements in the variable
+        document.getElementById("list-gyms").innerHTML = gyms;
+
+        initMap(coordinates);
+    });
+}
+
+//function to show the geolocation map
+function initMap(coordinates) 
+{
+    const map = new google.maps.Map(document.getElementById("map"), 
+    {
+        zoom: 14,
+        center: coordinates[0],
+    });
+
+    //for each to add the pins to the map
+    coordinates.forEach(coordinate => 
+    {
+        new google.maps.Marker({
+            position: coordinate,
+            map: map,
+        });
+    });
+}
